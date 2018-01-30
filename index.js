7ac4e382e488bf43d48f99840062e7631f27dac5
jack f perry jr <jackperryjr@cid.local>
2018-01-30 15:07:12 -0500

    Updated with tooltip styles and position

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 
 // <--- Created by Jack F. Perry, Jr. --->
 
+// <--- Function to help position and style the tooltips --->
+(function() {
+    var ID = "tooltip";
+    var CLS_ON = "tooltip_ON";
+    var FOLLOW = true;
+    var DATA = "_tooltip";
+    var OFFSET_X = -220;
+    var OFFSET_Y = 20;
+    
+    showAt = function(e) {
+        var ntop = e.pageY + OFFSET_Y, nleft = e.pageX + OFFSET_X;
+        $("#" + ID).html($(e.target).data(DATA)).css({
+            position: "absolute", top: ntop, left: nleft
+        }).show();
+    };
+    $(document).on("mouseenter", "*[title]", function(e) {
+        $(this).data(DATA, $(this).attr("title"));
+        $(this).removeAttr("title").addClass(CLS_ON);
+        $("<div id='" + ID + "' />").appendTo("body");
+        showAt(e);
+    });
+    $(document).on("mouseleave", "." + CLS_ON, function(e) {
+        $(this).attr("title", $(this).data(DATA)).removeClass(CLS_ON);
+        $("#" + ID).remove();
+    });
+    if(FOLLOW) { 
+      $(document).on("mousemove", "." + CLS_ON, showAt); 
+  }
+}());
+
 // <--- Angular component to fetch and control my character list --->
 var app = angular.module('charApp', []);
 
