ff2dc849729c84396d9195ec1bc3159dad70da8b
jack f perry jr <jackperryjr@cid.local>
2018-02-07 12:22:32 -0500

    Cleaned up and removed chocoDex code

--- index.js
+++ index.js
@@ -45,69 +45,6 @@ app.controller('characterController', function($scope, $http) {
     });
   });
 
-/*****************************************************
-
-// <--- Angular controller to manage the chocobos --->
-app.controller('chocoboController', function($scope, $http) {
-  $http.get('chocobos.json')
-    .then(function(response) {
-      $scope.chocobos = response.data;
-
-      // <--- Setting initial statuses --->
-      $scope.current = 0;
-      $scope.chocodex = false;
-      $scope.chococard = false;
-      $scope.chocoinfo = false;
-      $scope.chocoimage = false; 
-      $scope.chocobutton = false;   
-      $scope.floatingimage = true;
-
-      //<--- Function to hide floating image and show chocoDex --->
-      $scope.chocoDex = function() {
-        $scope.chocodex = true;
-        $scope.chococard = true;
-        $scope.chocoimage = true;
-        $scope.chocobutton = true;
-        $scope.chocoinfo = false;
-        $scope.floatingimage = false;
-      },
-
-      // <--- Function to show chocobo info --->
-      $scope.showInfo = function() {
-        $scope.chocoinfo = true; 
-        $scope.chocoimage = false;         
-      },
-
-      // <--- Function to show chocobo image --->
-      $scope.showImage = function() {
-        $scope.chocoimage = true;
-        $scope.chocoinfo = false;        
-      },
-
-      // <--- Function to move to the next chocobo --->
-      $scope.next = function() {
-        $scope.current = ($scope.current + 1) % $scope.chocobos.length;
-      },
-
-      // <--- Function to move to the previous chocobo --->
-      $scope.previous = function() {
-        $scope.current = ($scope.current - 1) % $scope.chocobos.length;
-        if ($scope.current < 0) {
-          $scope.current = 9;
-        }
-      },
-
-      // <--- Function to hide the chocoDex and show the floating image --->
-      $scope.back = function() {
-        $scope.chocodex = false;
-        $scope.chocobutton = false;
-        $scope.floatingimage = true;
-      }
-  });
-});
-
-*****************************************************/ 
-
 // <--- Angular controller to manage the monsters --->
 app.controller('monsterController', function($scope, $http) {
   $http.get('monsters.json')
