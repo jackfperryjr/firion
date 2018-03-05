970330507dbbcb7f4f8486810bdc762929ae472d
jack f perry jr <jackperryjr@cid.local>
2018-03-05 06:23:10 -0500

    A forgotten commit

--- index.js
+++ index.js
@@ -5,13 +5,13 @@
 let desktopLogo = $("#desktop");
 let mobileLogo = $("#mobile");
 let charSearch = $("#char-search");
+let charCard = $("#char-card");
 let input = $("#inputForm");
 let inputOpen = $("#inputOpen");
 let inputClose = $("#inputClose");
 let inputSubmit = $("#inputSubmit");
 let form = $("#form");
 let dataSent = $("#dataSent");
-
 const desktop = window.matchMedia("(min-width: 810px)");
 
 //<--- Hiding divs --->
@@ -46,7 +46,83 @@ $(document).ready(function() {
         dataSent.hide();
     });
 
+
+    /***********************************************************************
+    
+
+    const reducerFunction = (data, element) => {
+
+        // Add the current field to the object.
+        data[element.name] = element.value;
+
+        // For the demo only: show each step in the reducer’s progress.
+        console.log(JSON.stringify(data));
+
+        return data;
+    };
+
+    const formToJSON_deconstructed = elements => {
+  
+        // This is the function that is called on each element of the array.
+        const reducerFunction = (data, element) => {
+            
+            // Add the current field to the object.
+            data[element.name] = element.value;
+            
+            // For the demo only: show each step in the reducer’s progress.
+            console.log(JSON.stringify(data));
+
+            return data;
+        };
+  
+        // This is used as the initial value of `data` in `reducerFunction()`.
+        const reducerInitialValue = {};
+        
+        // To help visualize what happens, log the inital value.
+        console.log('Initial `data` value:', JSON.stringify(reducerInitialValue));
+        
+        // Now we reduce by `call`-ing `Array.prototype.reduce()` on `elements`.
+        const formData = [].reduce.call(elements, reducerFunction, reducerInitialValue);
+        
+        // The result is then returned for use elsewhere.
+        return formData;
+    };
+
+    const formToJSON = elements => [].reduce.call(elements, (data, element) => {
+  
+        data[element.name] = element.value;
+        return data;
+
+        }, {});
+
+    const handleFormSubmit = event => {
+  
+        // Stop the form from submitting since we’re handling that with AJAX.
+        event.preventDefault();
+        
+        // TODO: Call our function to get the form data.
+        const data = formToJSON(form.elements);
+        
+        // Demo only: print the form data onscreen as a formatted JSON object.
+        const dataContainer = document.getElementsByClassName('results__display')[0];
+        
+        // Use `JSON.stringify()` to make the output valid, human-readable JSON.
+        dataContainer.textContent = JSON.stringify(data, null, "  ");
+        
+        // ...this is where we’d actually do something with the form data...
+    };
+
+    const form = form[0];
+    form.addEventListener('submit', handleFormSubmit);
+
+
+
+
+    *************************************************************************/
     inputSubmit.click(function() {
+        //fs.appendFile("characters.json", obj, function (err) {
+        //    if (err) throw err;
+        //}),
         form[0].reset();
         dataSent.fadeIn(500);
     })
