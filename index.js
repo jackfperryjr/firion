a2dd9a60909d06405e2d1c46558191f3994d0501
jack f perry jr <jackperryjr@cid.local>
2018-04-02 18:06:47 -0400

    Refactored the comments

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-
-// <--- Created by Jack F. Perry, Jr. --->
+// <-- Created by Jack F. Perry, Jr. -->
+// =====================================
 
 // <--- Initializing Angular component --->
+// ========================================
 var app = angular.module('finalFantasyApp', []);
 
 // <--- Angular controller to manage the characters --->
+// =====================================================
 app.controller('characterController', function($scope, $http) {
   $http.get('characters.json')
     .then(function(response) {
       $scope.characters = response.data;
     });
 });
+// ====================
+// <-- That's that. -->
