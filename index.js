bfa2aeae41d4b125669b06dba1efd52ab2c64aac
jack f perry jr <jackperryjr@cid.local>
2018-04-25 12:07:51 -0400

    Shortened code for auto year

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ app.controller('characterController', function($scope, $http) {
 });
 // <-- Adding current year copyright --->
 // ======================================
-let today = new Date()
-let year = today.getFullYear()
+let year = new Date().getFullYear();
 document.getElementById('footer-name').innerHTML = "&copy; " + year + " jack f. perry, jr.";
 // ====================
 // <-- That's that. -->
