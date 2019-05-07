3a0c1abdd0a07fa3cfe5e28ef6ef9a77c842833d
jackfperryjr <jack.franklin.perryjr@gmail.com>
2019-05-07 07:43:51 -0400

    Added a keyup function for searching

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-var app = angular.module('finalFantasyApp', []); // Initializing Angular.Js component.
+$(document).ready(function(){
+  let year = new Date().getFullYear(); // Getting the current full year.
+  let search = $("#search");
+  search.on("keyup", function(){
+    if ($(this).val().length > 2) {
+      $("#char-card").show();
+    } else {
+      $("#char-card").hide();
+    }
+  }).keyup();
+  $("#footer-name").innerHTML = "&copy; " + year + " jack f. perry, jr."; // Adding full year to the copyright.
+})
 
-app.controller('characterController', function($scope, $http) { // Controller to manage characters.
-  $http.get('https://www.moogleapi.com/api/v1/characters')
+let app = angular.module('finalFantasyApp', []); // Initializing Angular.Js component.
+
+app.controller("characterController", function($scope, $http) { // Controller to manage characters.
+  $http.get("https://www.moogleapi.com/api/v1/characters")
     .then(function(response) {
       $scope.characters = response.data;
     });
 });
 
-let year = new Date().getFullYear(); // Getting the current full year.
-document.getElementById('footer-name').innerHTML = "&copy; " + year + " jack f. perry, jr."; // Adding full year to the copyright.
